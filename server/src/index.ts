f52770509e6566d09366a381cc7d405db7c55039
codeawy <codeawy@gmail.com>
2024-07-15 17:04:51 +0300

    TypeDefs

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,24 @@
-import { ApolloServer } from "apollo-server";
+import { ApolloServer, gql } from "apollo-server";
 
-const server = new ApolloServer({});
+// * TypeDefs
+const typeDefs = gql`
+  type User {
+    id: String
+    name: String
+  }
+
+  type Post {
+    id: String
+    title: String
+  }
+`;
+
+const server = new ApolloServer({
+  typeDefs,
+});
 
 const PORT = process.env.PORT || 5000;
 
 server.listen({ port: PORT }, () => {
-  console.log(`Server ready at http://localhost:5000`);
+  console.log(`Server ready at http://localhost:${PORT}`);
 });
