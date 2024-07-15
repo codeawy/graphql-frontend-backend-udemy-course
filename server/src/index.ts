faa924ab7ba00f61b3e11e68a02c3b5ef8984e14
codeawy <codeawy@gmail.com>
2024-07-15 17:29:38 +0300

    Resolvers

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,18 +3,34 @@ import { ApolloServer, gql } from "apollo-server";
 // * TypeDefs
 const typeDefs = gql`
   type User {
-    id: String
-    name: String
+    id: String!
+    name: String!
+    age: String
   }
 
   type Post {
     id: String
     title: String
   }
+
+  type Query {
+    getUsers: [User!]!
+    getPosts: [Post]
+  }
 `;
+
+const resolvers = {
+  Query: {
+    getUsers: [
+      { id: "1", name: "Codeawy" },
+      { id: "2", name: "Naga" },
+    ],
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
+  resolvers,
 });
 
 const PORT = process.env.PORT || 5000;
